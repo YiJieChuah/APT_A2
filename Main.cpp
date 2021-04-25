23346294dd9bcdbb9624704fd401e1c41ce299cd
[seth danford s3845408] <s3845408@student.rmit.edu.au>
2021-04-25 12:25:22 +1000

    Updated Classes

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,13 @@
 #include "Menu.cpp"
 
+using namespace std;
+
 int main(int argc, char const *argv[])
 {
     Menu *menu = new Menu();
+    int selection = menu->mainMenu();
+
+    cout << selection << endl;
+
     return 0;
 }
--- Menu.cpp
+++ Menu.cpp
@@ -1,21 +1,67 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
 
 /**
  * Present the user with the iterface. Also process user input here?
  */
 class Menu
 {
-private:
-    /* data */
+
 public:
-    Menu(/* args */);
+    Menu();
     ~Menu();
+    int mainMenu();
+
+    string *presentOption1();
 };
 
-Menu::Menu(/* args */)
+Menu::Menu()
 {
 }
 
 Menu::~Menu()
 {
+}
+
+/**
+ * @return the users choice.
+ */
+int Menu::mainMenu()
+{
+    int selection;
+    string strSelection;
+    cout << "Menu" << endl;
+    cout << "----" << endl;
+    cout << "1. New Game" << endl;
+    cout << "2. Load Game" << endl;
+    cout << "3. Credits (Show student information)" << endl;
+    cout << "4. Quit" << endl;
+
+    bool inputValid = false;
+    while (!inputValid)
+    {
+        cout << "\n>";
+        cin >> strSelection;
+        try
+        {
+            selection = stoi(strSelection);
+            inputValid = true;
+        }
+        catch (...)
+        {
+            cout << "Invalid Input";
+        }
+    }
+    return selection;
+}
+
+/**
+ * @return a pointer to a string array with the two users.
+ */
+string *Menu::presentOption1()
+{
+    string stuff[2] = {"miao", "cow"};
+    return stuff;
 }
