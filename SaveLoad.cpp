6e9df75184038bab35d5c6518b51a473147eb9fe
[seth danford s3845408] <s3845408@student.rmit.edu.au>
2021-05-07 23:51:36 +1000

    Update SaveLoad.cpp

--- SaveLoad.cpp
+++ SaveLoad.cpp
@@ -2,25 +2,40 @@
 #include <iostream>
 #include <string>
 #include <memory>
-
-using namespace std;
+#include <fstream>
 
 class SaveLoad
 {
 
 public:
-    bool save();
-    bool load(string fileName);
+    bool save(Board board, std::string fileName);
+    bool load(std::string fileName);
 };
 
-bool SaveLoad::save()
-
+bool SaveLoad::save(Board board, std::string fileName)
 {
+
+    try
+    {
+        std::ofstream saveFile(fileName + ".save");
+
+        if (saveFile.is_open())
+        {
+            saveFile << board.getSaveFormat();
+            saveFile.close();
+        }
+        else
+            std::cout << "There was a problem saving the game.";
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
     return false;
 }
 
-bool SaveLoad::load(string fileName)
+bool SaveLoad::load(std::string fileName)
 {
 
     return false;
